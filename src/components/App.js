f40e02a62503506805ea0ff6a263a0437dbd3742
Thomas-Deblay <thomasdeblay@pc-2-166.customer.ask4.lan>
2022-08-19 19:58:06 +0200

    Installation de Material UI. Creation de la sidebar Twitter avec ses sidebarOptions.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../css/App.css';
+import Sidebar from './Sidebar';
 
 const App = () => {
   return (
     <div className="app">
       <h1>Twitter Clone </h1>
-      {/* Sidebar  */}
+      <Sidebar />
       {/* Feed  */}
       {/* Widgets */}
     </div>
